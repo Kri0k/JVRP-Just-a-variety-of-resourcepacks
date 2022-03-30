1e8281994248deeb8dbb8b00e6e12c950eb650c0
Anton <68000966+Kri0k@users.noreply.github.com>
2022-03-30 15:39:38 +0400

    Add files via upload

---
+++ subsites/alcopack/alcopack.js
@@ -0,0 +1,34 @@
+var gallery = [
+    '../../resources/alcopack/blue_wine.png',
+    '../../resources/alcopack/glow_wine.png',
+    '../../resources/alcopack/green_wine.png',
+    '../../resources/alcopack/white_wine.png',
+    '../../resources/alcopack/red_wine.png',
+    '../../resources/alcopack/orange_wine.png',
+    '../../resources/alcopack/pink_wine.png',
+    '../../resources/alcopack/cola.png',
+    '../../resources/alcopack/more.png',
+    '../../resources/alcopack/showcase.png'
+]
+
+imgOne.apply(gallery);
+imgTwo.apply(gallery);
+imgThree.apply(gallery);
+imgFour.apply(gallery);
+imgFive.apply(gallery);
+imgSix.apply(gallery);
+imgSeven.apply(gallery);
+imgEight.apply(gallery);
+imgNine.apply(gallery);
+imgTen.apply(gallery);
+imgTen();
+function imgOne() {$('.gallery').css('background-image', 'url('+ gallery[0] +')');}
+function imgTwo() {$('.gallery').css('background-image', 'url('+ gallery[1] +')');}
+function imgThree() {$('.gallery').css('background-image', 'url('+ gallery[2] +')');}
+function imgFour() {$('.gallery').css('background-image', 'url('+ gallery[3] +')');}
+function imgFive() {$('.gallery').css('background-image', 'url('+ gallery[4] +')');}
+function imgSix() {$('.gallery').css('background-image', 'url('+ gallery[5] +')');}
+function imgSeven() {$('.gallery').css('background-image', 'url('+ gallery[6] +')');}
+function imgEight() {$('.gallery').css('background-image', 'url('+ gallery[7] +')');}
+function imgNine() {$('.gallery').css('background-image', 'url('+ gallery[8] +')');}
+function imgTen() {$('.gallery').css('background-image', 'url('+ gallery[9] +')');}
